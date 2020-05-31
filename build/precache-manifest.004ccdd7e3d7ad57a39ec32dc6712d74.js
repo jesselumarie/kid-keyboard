a10994ddd8657351e16960e82150ecc87d535ca0
Jesse Lumarie <jesse.lumarie@gmail.com>
2020-05-31 08:58:42 -0700

    Add build folder

---
+++ build/precache-manifest.004ccdd7e3d7ad57a39ec32dc6712d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31ec8e86974059813872b811e817402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52159276d1fd83626a1",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "0c958b04066d414e9027",
+    "url": "/static/js/2.722e36da.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.722e36da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52159276d1fd83626a1",
+    "url": "/static/js/main.96c5d5e8.chunk.js"
+  },
+  {
+    "revision": "69873d6c55a7570ce3e7",
+    "url": "/static/js/runtime-main.ac883119.js"
+  }
+]);
